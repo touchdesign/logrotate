cdeb432f4325fd3064181e07616368936729dfc1
Christin Gruber <c.gruber@touchdesign.de>
2020-10-31 15:56:35 +0100

    added examples

---
+++ examples/Purge.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author Christin Gruber
+ */
+require_once '../vendor/autoload.php';
+
+use Touchdesign\Logrotate\Loader\LogfileLoader;
+use Touchdesign\Logrotate\Worker\PurgeWorker;
+
+$purge = new PurgeWorker(
+    (new LogfileLoader('/tmp/logfile.log'))
+);
+
+$purge->run();
