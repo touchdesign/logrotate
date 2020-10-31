3c13c774e9a8e9714eac7688f1a9af74e17b7380
Christin Gruber <c.gruber@touchdesign.de>
2020-10-31 23:44:24 +0100

    added proto truncate test

---
+++ tests/TruncateTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Touchdesign\Logrotate\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Christin Gruber
+ */
+final class TruncateTest extends TestCase
+{
+
+    public function testSingle(): void
+    {
+        // @todo: implement me
+    }
+
+    public function testMultiple(): void
+    {
+        // @todo: implement me
+    }
+
+    public function testMode(): void
+    {
+        // @todo: implement me
+    }
+
+    public function testTruncateOrCreate(): void
+    {
+        // @todo: implement me
+    }
+
+    public function testInvalidArgumentException(): void
+    {
+        // @todo: implement me
+    }
+}
