7257aeb280f034c77e17927f7bd6d5696299ac62
Christin Gruber <c.gruber@touchdesign.de>
2020-11-01 11:44:52 +0100

    added truncate tests

--- tests/TruncateTest.php
+++ tests/TruncateTest.php
@@ -1,37 +1,82 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Touchdesign\Logrotate\Tests;
 
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
+use Touchdesign\Logrotate\Loader\LogfileLoader;
 
 /**
  * @author Christin Gruber
  */
 final class TruncateTest extends TestCase
 {
+    const CONTENT = 'HelloContent';
+    const BLANK = '';
+
+    /**
+     * @vfsStream virtual file system
+     */
+    private $filesystem;
+
+    public function setUp(): void
+    {
+        $this->filesystem = vfsStream::setup('root', 0644, [
+            'logs' => [
+                'foo.log' => self::CONTENT,
+                'bar.log' => self::CONTENT,
+            ],
+        ]);
+    }
 
     public function testSingle(): void
     {
-        // @todo: implement me
-    }
+        $this->assertStringEqualsFile($this->filesystem->url().'/logs/foo.log', self::CONTENT);
 
-    public function testMultiple(): void
-    {
-        // @todo: implement me
+        (new LogfileLoader($this->filesystem->url().'/logs/foo.log'))
+            ->truncate();
+
+        $this->assertFileExists($this->filesystem->url().'/logs/foo.log');
+        $this->assertStringEqualsFile($this->filesystem->url().'/logs/foo.log', self::BLANK);
     }
 
     public function testMode(): void
     {
-        // @todo: implement me
+        $this->assertStringEqualsFile($this->filesystem->url().'/logs/foo.log', self::CONTENT);
+
+        (new LogfileLoader($this->filesystem->url().'/logs/foo.log', 0777))
+            ->truncate();
+
+        $this->assertFileExists($this->filesystem->url().'/logs/foo.log');
+        $this->assertStringEqualsFile($this->filesystem->url().'/logs/foo.log', self::BLANK);
+
+        $mode = substr(sprintf('%o', fileperms($this->filesystem->url().'/logs/foo.log')), -4);
+        $this->assertSame($mode, '0777');
+        $this->assertEquals($mode, '0777');
     }
 
     public function testTruncateOrCreate(): void
     {
-        // @todo: implement me
+        $this->assertFileNotExists($this->filesystem->url().'/logs/bullshit.log');
+
+        (new LogfileLoader($this->filesystem->url().'/logs/bullshit.log'))
+            ->truncate();
+
+        $this->assertFileExists($this->filesystem->url().'/logs/bullshit.log');
+        $this->assertStringEqualsFile($this->filesystem->url().'/logs/bullshit.log', self::BLANK);
     }
 
-    public function testInvalidArgumentException(): void
+    public function testTypeErrorException(): void
     {
-        // @todo: implement me
+        $this->assertStringEqualsFile(
+            $this->filesystem->url().'/logs/foo.log',
+            self::CONTENT
+        );
+        $this->expectException(\TypeError::class);
+
+        (new LogfileLoader($this->filesystem->url().'/logs/foo.log', '-rw-rw-r--'));
+        (new LogfileLoader(90210, 0640));
     }
 }
